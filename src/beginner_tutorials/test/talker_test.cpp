8001c2852e0450315ed4361c00e9bfe60b7d5e07
rjb3vp <rjb3vp@umd.edu>
2019-11-11 11:03:33 -0500

    Making sure the init is called in our test

--- src/beginner_tutorials/test/talker_test.cpp
+++ src/beginner_tutorials/test/talker_test.cpp
@@ -18,8 +18,8 @@ TEST(TestSuite, testCase2)
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
-  //testing::InitGoogleTest(&argc, argv);
-  //ros::init(argc, argv, "tester");
+  testing::InitGoogleTest(&argc, argv);
+  //ros::init(argc, argv, "test_talker");
   //ros::NodeHandle nh;
   return RUN_ALL_TESTS();
 }
