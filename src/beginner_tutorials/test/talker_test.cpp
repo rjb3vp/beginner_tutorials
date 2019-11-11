4479be2b13a48813a35c67f150d5d42fa86b9493
rjb3vp <rjb3vp@umd.edu>
2019-11-11 08:24:06 -0500

    Tried to get trivial testing working, but cmake isn't making things correctly

--- src/beginner_tutorials/test/talker_test.cpp
+++ src/beginner_tutorials/test/talker_test.cpp
@@ -18,8 +18,8 @@ TEST(TestSuite, testCase2)
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
-  testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "tester");
-  ros::NodeHandle nh;
+  //testing::InitGoogleTest(&argc, argv);
+  //ros::init(argc, argv, "tester");
+  //ros::NodeHandle nh;
   return RUN_ALL_TESTS();
 }
