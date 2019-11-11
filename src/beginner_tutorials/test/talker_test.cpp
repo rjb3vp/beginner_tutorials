da58b194f26bb15d273617119d37a2542186e241
rjb3vp <rjb3vp@umd.edu>
2019-11-11 11:36:50 -0500

    Fleshed out test to test talker's service call

--- src/beginner_tutorials/test/talker_test.cpp
+++ src/beginner_tutorials/test/talker_test.cpp
@@ -3,23 +3,34 @@
 //#include "foo/foo.h"
 // Bring in gtest
 #include <gtest/gtest.h>
+#include "beginner_tutorials/SetRandomRange.h"
 
 // Declare a test
 TEST(TestSuite, testCase1)
 {
 //<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
+  ros::NodeHandle n;
+  ros::ServiceClient client =
+n.serviceClient<beginner_tutorials::SetRandomRange>("random_data");
+
+  beginner_tutorials::SetRandomRange srv;
+  srv.request.mean = 3;
+  srv.request.range = 2;
+  bool call = client.call(srv);
+  EXPECT_TRUE(call);
+
+  if (call) {
+    EXPECT_FALSE((bool)srv.response.error);
+    //ROS_INFO_STREAM("Errors detected?:" << (bool)srv.response.error);
+  }
+
 }
 
-// Declare another test
-TEST(TestSuite, testCase2)
-{
-//<test things here, calling EXPECT_* and/or ASSERT_* macros as needed>
-}
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
-  //ros::init(argc, argv, "test_talker");
-  //ros::NodeHandle nh;
+  ros::init(argc, argv, "test_talker");
+  ros::NodeHandle nh;
   return RUN_ALL_TESTS();
 }
